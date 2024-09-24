b0644b908b54f62d36d5e9d0ea3873ffb2a4431d
이은상 <kyrsang0801@inbrein.com>
2024-09-24 13:01:05 +0900

    Chore:

--- edge-service/src/main/java/com/example/edgeservice/config/SecurityConfig.java
+++ edge-service/src/main/java/com/example/edgeservice/config/SecurityConfig.java
@@ -3,6 +3,10 @@ package com.example.edgeservice.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.web.server.csrf.ServerCsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.server.csrf.XorServerCsrfTokenRequestAttributeHandler;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import org.springframework.context.annotation.Bean;
@@ -41,7 +45,10 @@ public class SecurityConfig {
 						.authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)))
 				.oauth2Login(Customizer.withDefaults())
 				.logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository)))
-				.csrf(csrf -> csrf.csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse()))
+				.csrf(csrf -> csrf
+						.csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
+						.csrfTokenRequestHandler(new SpaServerCsrfTokenRequestHandler())
+				)
 				.build();
 	}
 
@@ -61,5 +68,23 @@ public class SecurityConfig {
 			}));
 			return chain.filter(exchange);
 		};
+	}
+
+	static final class SpaServerCsrfTokenRequestHandler extends ServerCsrfTokenRequestAttributeHandler {
+		private final ServerCsrfTokenRequestAttributeHandler delegate = new XorServerCsrfTokenRequestAttributeHandler();
+
+		@Override
+		public void handle(ServerWebExchange exchange, Mono<CsrfToken> csrfToken) {
+			/*
+			 * Always use XorCsrfTokenRequestAttributeHandler to provide BREACH protection of the CsrfToken when it is rendered in the response body.
+			 */
+			this.delegate.handle(exchange, csrfToken);
+		}
+
+		@Override
+		public Mono<String> resolveCsrfTokenValue(ServerWebExchange exchange, CsrfToken csrfToken) {
+			final var hasHeader = exchange.getRequest().getHeaders().get(csrfToken.getHeaderName()).stream().filter(StringUtils::hasText).count() > 0;
+			return hasHeader ? super.resolveCsrfTokenValue(exchange, csrfToken) : this.delegate.resolveCsrfTokenValue(exchange, csrfToken);
+		}
 	}
 }
